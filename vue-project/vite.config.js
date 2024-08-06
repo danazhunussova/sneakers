824341da20134bdcf9867bfc547c4c6e7854449a
Dana <dana.zhunussova04@gmail.com>
2024-08-07 00:19:06 +0500

    trying...

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',  
+  base: '/', 
   plugins: [
     vue(),
     vueJsx(),
