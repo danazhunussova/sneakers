55c57569334258f8700d8e0a6be6e7247cfe5766
Dana <dana.zhunussova04@gmail.com>
2024-08-07 15:59:08 +0500

    trying

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
+  base: process.env.NODE_ENV === 'production' ? '/sneakers/' : '/',
   plugins: [
     vue(),
     vueJsx(),
