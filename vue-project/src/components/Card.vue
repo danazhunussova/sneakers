a52a6be2a7bac2bda25af58ffe44618d0fd7c484
Dana <dana.zhunussova04@gmail.com>
2024-08-07 14:02:20 +0500

    paths

--- vue-project/src/components/Card.vue
+++ vue-project/src/components/Card.vue
@@ -20,7 +20,7 @@
     <div class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:shadow-xl hover:-translate-y-2 transition"> 
       <img 
       @click="onClickFav" 
-      :src="isFav ? '/like-2.svg' : '../like-1.svg'" 
+      :src="isFav ? 'like-2.svg' : 'like-1.svg'" 
       alt="Like 1" 
       class="absolute top-8 left-8"/>
       
@@ -36,7 +36,7 @@
 
         <img 
         @click="onClickAdd" 
-        :src="isAdded ? '/checked.svg' : '../plus.svg' " 
+        :src="isAdded ? 'checked.svg' : 'plus.svg' " 
         alt="Plus">
       </div>
     </div>
